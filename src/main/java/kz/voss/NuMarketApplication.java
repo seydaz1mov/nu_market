ac25b55d27f5ce6f8f023c6eb6be147c0bb59e43
nurbol.seidazimov <nurbol.seidazimov@btsdigital.kz>
2019-10-05 14:53:01 +0600

    NUM-101 Implement main class of application

---
+++ src/main/java/kz/voss/NuMarketApplication.java
@@ -0,0 +1,11 @@
+package kz.voss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NuMarketApplication.class, args);
+    }
+}
